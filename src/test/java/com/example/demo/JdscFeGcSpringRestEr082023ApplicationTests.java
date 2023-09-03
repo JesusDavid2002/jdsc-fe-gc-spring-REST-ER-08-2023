263386b152911fa7b1e7538d3a2cdfd01a473c70
JesusDavid2002 <jesusdavidsepu@gmail.com>
2023-09-03 15:30:51 +0200

    ejercicios 1-4

---
+++ src/test/java/com/example/demo/JdscFeGcSpringRestEr082023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdscFeGcSpringRestEr082023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
